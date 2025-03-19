d93052726e0dfe8d419f25fba930aad757f6a1ab
adrhill <adrian.hill@mailbox.org>
2025-03-19 22:50:16 +0100

    Restructure code

--- src/PixelFlipper.jl
+++ src/PixelFlipper.jl
@@ -37,34 +37,8 @@ Computes pixel flipping curves.
     steps::Int = 20
 end
 
+include("result.jl")
 include("eval.jl")
-
-# Tiny results wrapper to dispatch plots on.
-struct PixelFlippingResult{T}
-    MIF::Vector{T}
-    LIF::Vector{T}
-end
-
-mif(res::PixelFlippingResult) = res.MIF
-lif(res::PixelFlippingResult) = res.LIF
-srg(res::PixelFlippingResult) = sum(max.(0, res.MIF - res.LIF)) / steps(res) # integrate area between MIF and LIF
-steps(res::PixelFlippingResult) = length(res.MIF) - 1
-
-function unicode_plot(res::PixelFlippingResult)
-    MIF = mif(res)
-    LIF = lif(res)
-    x = range(0, 100; length=length(MIF))
-    plt = lineplot(
-        x,
-        MIF;
-        title="Pixel flipping curve (SRG=$(srg(res)))",
-        name="MIF",
-        xlabel="Occlusion (%)",
-        ylabel="p",
-    )
-    lineplot!(plt, x, LIF; color=:cyan, name="LIF")
-    return plt
-end
 
 export PixelFlipping, evaluate
 export PixelFlippingResult
