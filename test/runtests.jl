d777bec43af90845d12ae68398740d6de679b25f
adrhill <adrian.hill@mailbox.org>
2025-04-08 18:39:43 +0200

    Exclude pre-release

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,10 +7,8 @@ using JET
     @testset "Code quality (Aqua.jl)" begin
         Aqua.test_all(PixelFlipper)
     end
-    if VERSION < v"1.12" # TODO: remove. As of PR #4, JET fails on pre-release builds.
-        @testset "Code linting (JET.jl)" begin
-            JET.test_package(PixelFlipper; target_defined_modules=true)
-        end
+    @testset "Code linting (JET.jl)" begin
+        JET.test_package(PixelFlipper; target_defined_modules=true)
     end
     # Write your tests here.
     @testset "GPU tests" begin
